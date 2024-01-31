f009a5f09a4f67a84bd6a3ffbb72c03415a7b8b5
Luise06 <lukno20006@gmail.com>
2024-01-31 08:55:55 +0100

    Overlay Vignette

---
+++ Assets/Script/TakeDamageOverlay.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class TakeDamageOverlay : MonoBehaviour
+{
+    /*public float intensity;
+    public Vignette vignette;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!vignette)
+        {
+            Debug.Log("vignette is empty");
+        }
+        else
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vignette.intensity.min = 0;
+        vignette.intensity.max = 0;
+    }*/
+}
